a0b40da349f79d12d120a6f2ea2e08331dd1e6d2
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-01-28 19:31:58 -0400

    project finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import "./App.css";
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import api from "./services/api";
 
 function App() {
   const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
 
-  const handleSearch = () => {
-    alert("Clicou!");
-    console.log(input);
-  };
+  async function handleSearch() {
+    if (input === "") {
+      alert("Preencha com algum CEP!");
+    }
+    try {
+      const res = await api.get(`${input}/json`);
+      setCep(res.data);
+      console.log(res.data);
+      setInput("");
+    } catch {
+      alert("Erro ao buscar!");
+      const input = (document.querySelector("#inputCEP").value = "");
+      // setInput('') pode ser assim tbm
+    }
+  }
 
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
       <div className="containerInput">
         <input
+          id="inputCEP"
           type="text"
           value={input}
           placeholder="Digite seu CEP..."
@@ -25,14 +39,17 @@ function App() {
           <FiSearch />
         </button>
       </div>
-
-      <main className="main">
-        <h2>CEP: 79003222</h2>
-        <span>Rua Teste</span>
-        <span>Complemento: </span>
-        <span>Vila Rosa</span>
-        <span>Campo Grande - MS</span>
-      </main>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>Logradouro: {cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
-//https://viacep.com.br/ws/01310930/json/
+//01310930/json/
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "https://viacep.com.br/ws/",
 });
+
+export default api;
